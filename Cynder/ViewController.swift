9787e0287bd73fcc5c2a37468ecfa2866069b98a
Kristie Huang <kristiehuang01@gmail.com>
2019-03-30 01:32:57 -0700

    Xcode initial commit

---
+++ Cynder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cynder
+//
+//  Created by Kristie Huang on 3/23/19.
+//  Copyright © 2019 Kristie Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
